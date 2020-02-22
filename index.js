adad80462c6b7afa9e5ca4cdac6663513a9363da
K.Kiyokawa <koichi20110068@gmail.com>
2020-02-22 17:24:53 +0900

    feat: enablet to get key, startIndex, endIndex
    nearest object of target

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
- * @param target_obj {Object}
- * @param target_map {String} 
+ * @param targetObj {Object}
+ * @param targetMap {String} 
  * @return {any}
  *
  * a string that presents stucture of object you want to dig.
@@ -15,47 +15,108 @@
  *     ]
  * }
  *
- * you can get animal.marmal.moles[0].name by
- * dig(obj, \`animal: {
- *   marmal: {
- *     moles: [
- *       { name: * }
- *     ]
+ * you can get animal.marmal.moles[1].name by
+ * dig(obj, { 
+ *   animal: {
+ *     marmal: {
+ *       moles: [
+ *         {},
+ *         { name: '*' }
+ *       ]
+ *     }
  *   }
- * }\`)
+ * })
+ *
+ * you can customize the marker '*' as you want.
  */
-export function dig(target_obj, target_map) {
-  validateBrackets(target_map)
-
-  if (!existsTarget) {
-    throw Error('target not found error: you should mark `*` as target')
+export function dig(targetObj, targetMap, targetMarker = '*') {
+  if (targetMarker.match(/^[a-zA-Z]*$/g)) {
+    throw Error('target marker should not be composed of only alphabet')
   }
 
-  const next_target_map = ''
+  // e.g. {"animal":{"marmal":{"moles":[{},{"name":"*"}]}}}
+  let strMap = JSON.stringify(targetMap)
 
-  return dig(target_obj[key], next_target_map)
+  if (countWordInText(targetMarker, target)) {
+    throw Error(`target not found error: you should mark ${targetMarker} as target`)
+  }
+
+
+  const keys = []
+
+  while (true) {
+
+    if (strMap === targetMarker) break
+  }
+
+
+  let value
+  for (const key of keys.reverse()) {
+    value = value[key]
+  }
+  return value
+}
+
+/**
+ * @param strTarget {Object or Array} e.g. { name: '*' }
+ * @param targetMarker {String} e.g. '*'
+ * @return {String} key name e.g. 'name'
+ *         {Integer} object's start index after JSON.stringify e.g. 0
+ *         {Integer} object's  end  index after JSON.stringify e.g. 11
+ */
+export function getNearestObjectKeyAndIndexStartToEnd(target, targetMarker) {
+  const strTarget = JSON.stringify(target)
+  // e.g.
+  // {"animal":{"marmal":{"moles":[{},{"name":"*"}]}}}
+  //                                          ^ firstTargetIndex                                      
+  const firstTargetIndex = strTarget.indexOf(`"${targetMarker}"`)
+  const searchingIndex = firstTargetIndex - 1
+
+  switch (strTarget.charAt(searchingIndex)) {
+    case ':':
+      // targetMarker is included in object.
+
+      // e.g.
+      // {"animal":{"marmal":{"moles":[{},{"name":"*"}]}}}
+      //                                  ^ braceStartIndex
+      const braceStartIndex = strTarget.lastIndexOf('{',searchingIndex)
+
+      // e.g.
+      // {"animal":{"marmal":{"moles":[{},{"name":"*"}]}}}
+      //                                             ^ braceEndIndex
+      const braceEndIndex = strTarget.indexOf('}', searchingIndex)
+
+      // e.g. { name: '*' }
+      const includedObject = JSON.parse(strTarget.substring(braceStartIndex, braceEndIndex + 1))
+
+      for (const [key, value] of Object.entries(includedObject)) {
+        if (value === targetMarker) {
+          return [key, braceStartIndex, braceEndIndex]
+        }
+      }
+
+
+    case ',':
+    case '[':
+      // targetMarker is included in array. e.g. { animal :{ marmal :{ moles :[{}, '*']}}}
+
+      // e.g. {"animal":{"marmal":{"moles":[{},"*"]}}}
+      //                                   ^ bracketStartIndex
+      const bracketStartIndex = strTarget.lastIndexOf('[', searchingIndex)
+
+      // e.g. {"animal":{"marmal":{"moles":[{},"*"]}}}
+      //                                           ^ bracketEndIndex
+      const bracketEndIndex = strTarget.indexOf(']',searchingIndex)
+
+      // e.g. [{}, '*']
+      const includedArray = JSON.parse(strTarget.substring(bracketStartIndex, bracketEndIndex + 1))
+
+      const targetIndex = includedArray.indexOf(targetMarker)
+
+      return [targetIndex, bracketStartIndex, bracketEndIndex]
+  }
 }
 
 export function countWordInText(word, text) {
   return text.split(word).length - 1
 }
-
-// validate that the number of bracketStart equals to bracketEnd
-export function validateBrackets(target_map) {
-  const BRACKETS = [
-    ['{', '}'],
-    ['[', ']']
-  ]
-  for (const [bracketStart, bracketEnd] of BRACKETS) {
-    const bracketStartCount = countWordInText(bracketStart, target_map)
-    const bracketEndCount = countWordInText(bracketEnd, target_map)
-    if (bracketStartCount !== bracketEndCount) {
-      // return false
-      throw Error(`parse error: brackets are not corresponding. ${bracketStart} is ${bracketStartCount}, but ${bracketEnd} is ${bracketEndCount}`)
-    }
-  }
-}
-
-  function existsTarget(target_map) {
-    return countWordInText('*', target_map) >= 1
-  }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,7 @@
 import {
   dig,
-  validateBrackets,
-  countWordInText
+  countWordInText,
+  getNearestObjectKeyAndIndexStartToEnd
 } from "../index"
 
 describe('dig object', () => {
@@ -19,17 +19,70 @@ describe('dig object', () => {
     }
   }
 
-  const target_map = `animal: { marmal: moles: [{name: *}] }`
+  const targetMap = {
+    animal: {
+      marmal: {
+        moles: [{},
+          {
+            name: '***'
+          }
+        ]
+      }
+    }
+  }
 
   test('word count', () => {
-    expect(countWordInText('*', '***')).toBe(3)
-    expect(countWordInText('{', target_map)).toBe(2)
-    expect(countWordInText('}', target_map)).toBe(2)
-    expect(countWordInText('[', target_map)).toBe(1)
-    expect(countWordInText(']', target_map)).toBe(1)
+    expect(countWordInText('***', '***|***')).toBe(2)
+    expect(countWordInText('{', JSON.stringify(targetMap))).toBe(5)
+    expect(countWordInText('}', JSON.stringify(targetMap))).toBe(5)
+    expect(countWordInText('[', JSON.stringify(targetMap))).toBe(1)
+    expect(countWordInText(']', JSON.stringify(targetMap))).toBe(1)
   })
 
-  test('brackets are valid', () => {
-    expect(() => { validateBrackets(target_map) })
+  test('get nearest key from object', () => {
+    const [key, startIndex, endIndex] = getNearestObjectKeyAndIndexStartToEnd({
+      name: '*'
+    }, '*')
+    expect(key).toBe('name')
+    // {"name":"*"}
+    // 012345678901
+    expect(startIndex).toBe(0)
+    expect(endIndex).toBe(11)
   })
+
+  test('get nearest key from nested object', () => {
+    const [key, startIndex, endIndex] = getNearestObjectKeyAndIndexStartToEnd({
+      first: {
+        name: '*'
+      },
+      second: {
+        name: 'dummy'
+      }
+    }, '*')
+    expect(key).toBe('name')
+    // {"first":{"name":"*"},"second":{"name":"dummy"}}
+    //          ^          ^
+    // 012345678901234567890123456789012345678901234567
+    expect(startIndex).toBe(9)
+    expect(endIndex).toBe(20)
+  })
+
+  test('get nearest key from array', () => {
+    const [key, startIndex, endIndex] = getNearestObjectKeyAndIndexStartToEnd([{}, '*'], '*')
+    expect(key).toBe(1)
+    // [{},"*"]
+    // 01234567
+    expect(startIndex).toBe(0)
+    expect(endIndex).toBe(7)
+  })
+
+  test('get nearest key from array', () => {
+    const [key, startIndex, endIndex] = getNearestObjectKeyAndIndexStartToEnd(['*', {}], '*')
+    expect(key).toBe(0)
+    // ["*",{}]
+    // 01234567
+    expect(startIndex).toBe(0)
+    expect(endIndex).toBe(7)
+  })
+
 })
